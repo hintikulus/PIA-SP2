7930a597da433d2263bd71f604bbb6ca618f6856
Jan Hinterholzinger <hintik@icloud.com>
2023-10-13 12:22:38 +0200

    Vendor clean

--- vendor/contributte/mailing/src/Exception/LogicException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace Contributte\Mailing\Exception;
-
-class LogicException extends \LogicException
-{
-
-}
--- vendor/contributte/pdf/src/Exceptions/InvalidArgumentException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace Contributte\PdfResponse\Exceptions;
-
-class InvalidArgumentException extends LogicalException
-{
-
-}

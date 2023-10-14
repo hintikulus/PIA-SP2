8e917d3f17339997785bc61eb277f289d627d6ff
Jan Hinterholzinger <hintik@icloud.com>
2023-10-14 02:30:24 +0200

    Scripty pro datagrid

--- app/UI/Components/User/UserListGrid.php
+++ app/UI/Components/User/UserListGrid.php
@@ -5,24 +5,30 @@ namespace App\UI\Components\User;
 use App\Domain\User\UserFacade;
 use App\UI\Components\Base\BaseComponent;
 use App\UI\DataGrid\BaseGrid;
+use Contributte\Translation\Translator;
 
 class UserListGrid extends BaseComponent
 {
     private UserFacade $userFacade;
+    private Translator $translator;
 
     public function __construct(
         UserFacade $userFacade,
+        Translator $translator,
     )
     {
         $this->userFacade = $userFacade;
+        $this->translator = $translator;
     }
 
     public function createComponentGrid(): BaseGrid
     {
         $grid = new BaseGrid();
-        $grid->setDataSource($this->userFacade->getAll());
+        $grid->setTranslator($this->translator);
+        $grid->setDataSource($this->userFacade->getAllQueryBuilder());
         $grid->addColumnText('id', 'ID');
-
+        $grid->addColumnText('name', 'name');
+        $grid->addColumnText('emailAddress', 'E-mail');
 
         return $grid;
     }

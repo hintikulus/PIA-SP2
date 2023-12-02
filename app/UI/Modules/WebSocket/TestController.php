9c02253a0d314b57ae81b35ab6cb51aedfc33952
Jan Hinterholzinger <hintik@icloud.com>
2023-12-02 21:53:39 +0100

    Zprovoznění websocket komunikace - HURÁ

--- app/UI/Modules/WebSocket/TestController.php
+++ app/UI/Modules/WebSocket/TestController.php
@@ -1,36 +1,39 @@
 <?php
 
-namespace App\UI\Modules\WebSocket\Test;
+namespace App\UI\Modules\WebSocket;
 
-use App\UI\Modules\WebSocket\BaseWebSocketController;
 use IPub\WebSocketsWAMP\Entities\Clients\IClient;
 use IPub\WebSocketsWAMP\Entities\Topics\ITopic;
+use Nette\Security\User;
 use Nette\Utils\Json;
 use Tracy\Debugger;
 use Tracy\ILogger;
 
 class TestController extends BaseWebSocketController
 {
+
     /**
      * Akce která se zavolá když JS klient zavolá publish metodu.
      *
-     * @param \stdClass $event	- Zde jsou všechny příchozí data odeslané z JS klienta
      * @param IClient $client	- Klient který akci vyvolal
      * @param ITopic $topic		- Topic kterého se akce týká
      */
-    public function actionPublish(\stdClass $event, IClient $client, ITopic $topic)
+    public function actionPublish(array $event, IClient $client, ITopic $topic)
     {
         $outgoing = new \stdClass();
         $outgoing->time = (new \DateTime())->format('Y-m-d H:i:s');
         $outgoing->from = $client->getId();
-        $outgoing->content = $event->message;
+        $outgoing->content = $event['message'];
 
-        Debugger::log($event->message, ILogger::DEBUG);
+        Debugger::log($outgoing->content, ILogger::DEBUG);
 
         // Odešleme všem, i tomu kdo zprávu poslal
-        $topic->broadcast(Json::encode($outgoing));
+        //$topic->broadcast(Json::encode($outgoing));
 
         // Pošleme všem, vyjma toho kdo zprávu poslal
         $topic->broadcast(Json::encode($outgoing), [$client->getId()]);
+
+
+        var_dump($client->getId());
     }
 }

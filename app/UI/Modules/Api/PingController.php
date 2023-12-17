44ce11353661f29c845c94b4a3498104e0cd8271
Jan Hinterholzinger <hintik@students.zcu.cz>
2023-12-17 10:27:40 +0100

    Počáteční implementace REST API

---
+++ app/UI/Modules/Api/PingController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\UI\Modules\Api;
+
+use Nette\Application\UI\Presenter;
+
+use Contributte\ApiRouter\ApiRoute;
+use Nette\Application\Request;
+use Nette\Application\Response;
+use Nette\Application\Responses\TextResponse;
+
+/**
+ * API for ping/pong
+ *
+ * @ApiRoute(
+ * 	"/api/ping",
+ * 	methods={
+ * 		"GET"="run"
+ * 	},
+ *  presenter="Api:Ping",
+ *  format="plain"
+ * )
+ */
+final class PingController extends Presenter
+{
+
+    public function run(Request $request): Response
+    {
+        return new TextResponse('pong');
+    }
+}
